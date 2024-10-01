fbabd7d7997f4280ce0998e43e6627fa7275c203
Chris W <95181085+ChrisDev18@users.noreply.github.com>
2024-10-01 15:40:03 +0200

    Add type annotations, get all strings from HTML

--- parsing.py
+++ parsing.py
@@ -1,11 +1,14 @@
 from bs4 import BeautifulSoup
 
-def get_title_and_text(html):
+# get the title of the HTML
+def get_title_and_text(html: bytes) -> (str, list[str]):
     soup = BeautifulSoup(html, 'html.parser')
 
     title = soup.find('title').get_text()
     
-    paragraphs = soup.find_all('p')
-    article_text = ' '.join([para.get_text() for para in paragraphs])
+    # paragraphs = soup.find_all('p')
+
+    # article_text = ' '.join([para.get_text() for para in paragraphs])
+    strings = soup.body.strings # Get all strings from within the HTML
     
-    return title, article_text
+    return title, strings
--- scraping.py
+++ scraping.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_article_simple(url):
+
+def get_article_simple(url) -> bytes:
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
         'Accept-Language': 'en-US,en;q=0.9',
