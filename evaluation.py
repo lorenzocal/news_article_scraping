5dde3076c44d57c3cfb034cde8672d3cb19cabba
francesco boldrin <francesco.boldrin@studenti.unitn.it>
2024-10-01 15:32:07 +0200

    Added some comment and evaluation file

---
+++ evaluation.py
@@ -0,0 +1,16 @@
+'''
+Evaluation of the program with the ground truth data
+'''
+
+
+def distance_between_text():
+    pass
+
+def load_ground_truth_data():
+    pass
+
+def load_data():
+    pass
+
+def evaluate():
+    pass
