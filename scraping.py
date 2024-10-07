3fe1255c24495b005dbaa9fd2d2af8668ea7fb53
francesco boldrin <francesco.boldrin@studenti.unitn.it>
2024-10-07 14:57:38 +0200

    adjust the encoding

--- scraping.py
+++ scraping.py
@@ -28,7 +28,7 @@ def get_article_simple(url) -> bytes:
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
         'Accept-Language': 'en-US,en;q=0.9',
-        'Accept-Encoding': 'gzip, deflate, br',
+        # 'Accept-Encoding': 'gzip, deflate, br',
         'Connection': 'keep-alive',
         'Referer': 'https://www.google.com/'
     }
