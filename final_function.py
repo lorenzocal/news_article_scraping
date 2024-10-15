359e4dd4d471dad44ad9e60aa0919beb2b7032f8
Lorenzo Calogiuri <126566979+lorenzocal@users.noreply.github.com>
2024-10-15 17:19:32 +0200

    Create final_function.py

---
+++ final_function.py
@@ -0,0 +1,94 @@
+import pandas as pd
+from sklearn.preprocessing import RobustScaler
+# Run !pip install sentence_transformers before
+from sentence_transformers import SentenceTransformer 
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+# Load pre-trained Sentence-BERT model
+model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+
+# Function to compute coherence within a textual input
+def compute_coherence(title, text):
+
+    # Title and text are joined and processed together
+    text = " ".join([title, text])
+
+    # Split text into sentences (other techniques may be used for splitting)
+    sentences = text.split(".")
+    sentences = [sentence.strip() for sentence in sentences if sentence]
+
+    # Get embeddings for each sentence
+    embeddings = model.encode(sentences)
+
+    # Compute cosine similarities between consecutive sentences
+    similarities = []
+    for i in range(len(embeddings) - 1):
+        similarity = cosine_similarity([embeddings[i]], [embeddings[i + 1]])[0][0]
+        similarities.append(similarity)
+
+    # Average similarity score (overall coherence)
+    avg_similarity = np.mean(similarities)
+    
+    return avg_similarity
+
+
+def get_title_text(url):
+  
+  # List of function definied in the parsing script
+  bag_of_functions = [get_title_and_text1, get_title_and_text2, get_title_and_text3]
+
+  # Two fixed weights are defined for both coherence and abundance
+  # We give more importance to the coherence in the text
+  ABUNDANCE_WEIGHT = 0.3
+  COHERENCE_WEIGHT = 0.7
+
+  # Creating a new dataframe for storing results
+  results = pd.DataFrame()
+
+  html = get_article_simple(url)
+  
+  for function in bag_of_functions:
+    
+    title, text = function(html)
+    coherence = compute_coherence(title, text)
+    abundance = len(text)
+
+    # New result row to be added to the dataframe
+    new_row = pd.DataFrame([{'title': title, 'text': text, 'coherence': coherence, 'abundance': abundance}])
+
+    # Concatenate the new row with the existing DataFrame
+    results = pd.concat([results, new_row], ignore_index=True)
+
+  # Apply Robust Scaling to 'abundance' column
+  scaler = RobustScaler()
+  results['abundance'] = scaler.fit_transform(results[['abundance']])
+
+  # Assign a rank score to coherence and abundance (the higher the better)
+  results['coherence_rank'] = results['coherence'].rank(ascending=True, method='dense').astype(int)
+  results['abundance_rank'] = results['abundance'].rank(ascending=True, method='dense').astype(int)
+
+  # The final rank is the weighted average of the two ranks
+  results['final_rank'] = COHERENCE_WEIGHT * results['coherence_rank'] + ABUNDANCE_WEIGHT * results['abundance_rank']
+
+
+  # Get the index of the row with the highest 'final_rank'
+  max_rank_index = results['final_rank'].idxmax()
+
+  best_title = results.loc[max_rank_index, 'title']
+  best_text = results.loc[max_rank_index, 'text']
+
+  return best_title, best_text
+
+
+url_list = ['https://www.nytimes.com/2024/09/29/us/north-carolina-helene-relief-damage.html',
+            'http://www.chinatoday.com.cn/ctenglish/2018/commentaries/202409/t20240925_800378506.html',
+            'https://english.elpais.com/economy-and-business/2024-10-02/from-failed-inheritances-to-bad-investments-millionaires-who-lost-a-fortune.html']
+
+for url in url_list:
+  title, text = get_title_text(url)
+  print("-----")
+  print('Title: ' + title)
+  print('Text: ' + text)
+  print('-----')
+
