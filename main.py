f426457bcb679528ae30d13a75753bf277976cc7
francesco boldrin <francesco.boldrin@studenti.unitn.it>
2024-10-21 17:23:25 +0200

    minor changes

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def scrape_text_and_save(url_index):
     # title, texts = parsing.get_title_and_text1(html)  # step 2
 
     # print then store the text to a file, so it is easier to read
-    return print_retrieve_text(url_index, title, texts)
+    return print_and_save_retrieved_text(url_index, title, texts)
 
 def print_evaluation_in_json(evaluation, index):
     # construct the dictionary as
@@ -59,27 +59,35 @@ def print_evaluation_in_json(evaluation, index):
         url_name: evaluation
     }
 
-    # append the dictionary in the json file
-    with open("./evaluations/evaluations.json", "a") as file:
-        json.dump(result, file, indent=4)
+    file_path = "./evaluations/evaluations.json"
+    try:
+        with open("./evaluations/evaluations.json", "r") as file:
+            data = json.load(file)
+            data["evaluations"].append(result)
 
-# index = 1
-# scraped_path = scrape_text_and_save(index)
+        with open(file_path, "w") as file:
+            json.dump(data, file, indent=4)
+    except FileNotFoundError:
+        with open(file_path, "w") as file:
+            json.dump({"evaluations": [result]}, file, indent=4)
 
-# #  Evaluate the text
-# # TODO: parse and decide how to plot the evaluation
-# gt_path = "./data/GroundTruth/0{}.txt".format(index+1)
-# eval = evaluation.evaluate(gt_path, scraped_path)
+index = 1
+scraped_path = scrape_text_and_save(index)
 
-# print("Evaluation:")
-# print(eval)
+#  Evaluate the text
+# TODO: parse and decide how to plot the evaluation
+gt_path = "./data/GroundTruth/0{}.txt".format(index+1)
+eval = evaluation.evaluate(gt_path, scraped_path)
 
-# print_evaluation_in_json(eval, index)
+print("Evaluation:")
+print(eval)
 
-#=======================================================================================================================
+print_evaluation_in_json(eval, index)
+
+# =======================================================================================================================
 # GET ALL ARTICLES AND SAVE THEM TO TEXT
 # url_list = get_url_list()
-
+#
 # for i, url in enumerate(url_list) :
 #     print(f"Processing URL {i}: {url}")
 #     title, text = final_function.get_optimal_title_text(url)
